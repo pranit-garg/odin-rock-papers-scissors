79c7c0e9633ed67ca74e1379145d146973be2b22
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-08-27 21:06:50 +0800

    add logic for player and computer selection and what happens in each case

--- javascript.js
+++ javascript.js
@@ -7,8 +7,11 @@
 
 let choices = ["Rock", "Paper", "Scissors"]
 let computerSelection = getComputerChoice();
-let playerSelection  = "";
-
+let playerSelection  = prompt("Rock, Paper, Scissors? ");
+let rockRegex = /rock/i;
+let paperRegex = /paper/i;
+let scissorsRegex = /scissors/i;
+// console.log(computerSelection);
 
      function getComputerChoice() {
         let computerChoice = choices[Math.floor(Math.random()*choices.length)];
@@ -17,6 +20,34 @@ let playerSelection  = "";
 
 
      function playRound(playerSelection, computerSelection) {
+        if (rockRegex.test(playerSelection)) {
+            if (computerSelection == "Rock") {
+                return console.log("It's a draw! You both had Rock");
+            } else if (computerSelection == "Paper") {
+                return console.log("You lose! Paper beats Rock");
+            } else if (computerSelection == "Scissors") {
+                return console.log("You win! Rock beats Scissors");
+            }
 
+        } else if (paperRegex.test(playerSelection)) {
+            if (computerSelection == "Rock") {
+                return console.log("You win! Paper beats Rock");
+            } else if (computerSelection == "Paper") {
+                return console.log("It's a draw! You both had Paper");
+            } else if (computerSelection == "Scissors") {
+                return console.log("You lose! Scissors beat Paper");
+            }
+
+        } else if (scissorsRegex.test(playerSelection)) {
+            if (computerSelection == "Rock") {
+                return console.log("You lose! Rock beats Scissors");
+            } else if (computerSelection == "Paper") {
+                return console.log("You win! Scissors beats Paper");
+            } else if (computerSelection == "Scissors") {
+                return console.log("It's a draw! You both had Scissors");
+            }
+
+        }
      }
 
+playRound(playerSelection, computerSelection);
