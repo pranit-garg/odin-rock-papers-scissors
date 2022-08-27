b69dc78115d605c56ffadc63987c71e3e2d6f3f9
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-08-27 20:21:25 +0800

    add comments with how the game will work

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// write function for player choice that prompts user to choose rock, paper, or scissors
+    // it should take two inputs, player's choice and computer's choice
+        //player's choice should be case insenstive
+    // function should have a function within it for computer choice
+     // write function for computer choice that randomly chooses rock, paper, or scissors
+
