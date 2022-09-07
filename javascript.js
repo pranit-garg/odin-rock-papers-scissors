126049097526a8dc2eae1017b28899d1a1270887
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-07 13:03:30 +0800

    add some fixes for prompt looping, results still not logging properly

--- javascript.js
+++ javascript.js
@@ -9,42 +9,60 @@ let choices = ["Rock", "Paper", "Scissors"]
 let computerSelection = getComputerChoice();
 
 //wil need to come back to see how this goes into playround
-let playerSelection  = prompt("Rock, Paper, Scissors? ");
+// let playerSelection  = prompt("Rock, Paper, Scissors? ");
 let rockRegex = /rock/i;
 let paperRegex = /paper/i;
 let scissorsRegex = /scissors/i;
 let result = "";
 // console.log(computerSelection);
 
+    function getPlayerChoice() {
+        prompt("Rock, Paper, Scissors? ")
+    }
+
      function getComputerChoice() {
         let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+        // console.log(computerChoice);
         return computerChoice;
      }
 
-
+     let playerScore = 0;
+     let computerScore = 0;
      function playRound(playerSelection, computerSelection) {
         if (rockRegex.test(playerSelection)) {
             if (computerSelection == "Rock") {
                 return result = "It's a draw! You both had Rock";
             } else if (computerSelection == "Paper") {
+                // add score to computer since it won
+                computerScore++;
                 return result = "You lose! Paper beats Rock";
             } else if (computerSelection == "Scissors") {
+                // add score to player since you won
+                playerScore++;
                 return result = "You win! Rock beats Scissors";
             }
 
         } else if (paperRegex.test(playerSelection)) {
             if (computerSelection == "Rock") {
+                // add score to player since you won
+                playerScore++;
                 return result = "You win! Paper beats Rock";
             } else if (computerSelection == "Paper") {
                 return result = "It's a draw! You both had Paper";
             } else if (computerSelection == "Scissors") {
+                // add score to computer since you won
+                computerScore++;
                 return result = "You lose! Scissors beat Paper";
             }
 
         } else if (scissorsRegex.test(playerSelection)) {
             if (computerSelection == "Rock") {
+                // add score to computer since you won
+                computerScore++;
                 return result = "You lose! Rock beats Scissors";
             } else if (computerSelection == "Paper") {
+                // add score to player since you won
+                playerScore++;
                 return result = "You win! Scissors beats Paper";
             } else if (computerSelection == "Scissors") {
                 return result = "It's a draw! You both had Scissors";
@@ -53,10 +71,14 @@ let result = "";
      }
 
      function game() {
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 2; i++) {
+            console.log(result);
+            getPlayerChoice();
             playRound();
-            playRound();
+            // console.log("Computer Score:", computerScore, "Your Score:", playerScore);
         }
      }
+
+     game();
 
 // console.log(playRound(playerSelection, computerSelection));
